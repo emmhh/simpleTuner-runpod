23ffb1c8630d34be51dfc2bfc8761191c6b7f40e
dcanady <dcanady@debbie.debbie>
2024-11-10 14:25:54 -0500

    removed Jupyter password from docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -72,13 +72,11 @@ export_env_vars() {
 
 # Start jupyter lab
 start_jupyter() {
-    if [[ $JUPYTER_PASSWORD ]]; then
-        echo "Starting Jupyter Lab..."
-        mkdir -p /workspace && \
-        cd / && \
-        nohup jupyter lab --allow-root --no-browser --port=8888 --ip=* --FileContentsManager.delete_to_trash=False --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace &> /jupyter.log &
-        echo "Jupyter Lab started"
-    fi
+    echo "Starting Jupyter Lab..."
+    mkdir -p /workspace && \
+    cd / && \
+    nohup jupyter lab --allow-root --no-browser --port=8888 --ip=* --FileContentsManager.delete_to_trash=False --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token='' --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace &> /jupyter.log &
+    echo "Jupyter Lab started"
 }
 
 # ---------------------------------------------------------------------------- #
